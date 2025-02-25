3be14f1d774775ee53a05d2553d39a38ff68d9f5
Caio <caiodib10@gmail.com>
2025-02-25 12:52:19 -0300

    ex02

---
+++ exercicios1_estrutura_sequencial/src/exercicios1_estrutura_sequencial/ex02.java
@@ -0,0 +1,23 @@
+package exercicios1_estrutura_sequencial;
+
+import java.util.Scanner;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		System.out.println("Exercício 2");
+		Double pi = 3.14159;
+
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o valor do raio: ");
+		Double R = sc.nextDouble();
+		Double A = pi * R * R;
+
+		
+		System.out.printf("Área = %.4f%n", A);
+		
+
+	}
+
+}
